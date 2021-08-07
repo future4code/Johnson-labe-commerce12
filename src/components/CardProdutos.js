0e5263e915b32931d0c6f55990e130a2ff58be6a
Camila Azevedo Santos <ivanilojuior@live.com>
2021-08-06 23:04:48 -0300

    Comit 5

--- src/components/CardProdutos.js
+++ src/components/CardProdutos.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { IconeCarrinho } from './IconeCarrinho';
+import iconecarrinho from '../icones/iconecarrinho.png';
 
 const DivPrincipal= styled.div`
 height: 100%;
@@ -9,19 +11,25 @@ margin: 0;
 padding: 0;
 `
 const DivCard = styled.div`
-display: flex;
+display: grid;
 flex-direction: column;
 justify-content: center;
 text-align: center;
 align-items: center;
 `
 const Nome = styled.h5`
+display: flex;
 margin:0;
 padding: 0;
 `
 const Preco = styled.p`
+display: flex;
 margin: 0;
 padding: 0;
+`
+const DivIncone = styled.div`
+display: flex;
+flex-direction: row;
 `
 export default class CardProdutos extends React.Component{
 
@@ -32,9 +40,12 @@ export default class CardProdutos extends React.Component{
       return(
           <DivPrincipal>
               <DivCard>
-              <img src = {this.props.imagemProduto}></img>
-              <Nome>{this.props.nomeProduto}</Nome>
-              <Preco>{this.props.precoProduto}</Preco>
+                      <img src = {this.props.imagemProduto}></img>
+                      <Nome>{this.props.nomeProduto}</Nome>
+                      <Preco>{this.props.precoProduto}</Preco>
+                      <DivIncone>
+                      <IconeCarrinho icone = {iconecarrinho} ></IconeCarrinho>
+                      </DivIncone>
               </DivCard>
           </DivPrincipal>
           
